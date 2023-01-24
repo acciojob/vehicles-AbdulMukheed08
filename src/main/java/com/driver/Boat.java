e707301473f39e66b048dde82d63632c6d039873
Abdul Mukheed <you@example.com>
2023-01-24 17:45:47 +0530

    Vehicles Solution 5

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,38 @@
+package com.driver;
+
+class Boat implements WaterVehicle{
+
+    String name;
+    int capacity;
+
+    Boat(String name,int capacity){
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    @Override
+    public String getVehicleName() {
+        return name;
+    }
+
+    @Override
+    public int getVehicleCapacity() {
+        return capacity;
+    }
+
+    public java.lang.String getName() {
+        return name;
+    }
+
+    public void setName(java.lang.String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+}
--- src/main/java/com/driver/WaterVehicle.java
+++ src/main/java/com/driver/WaterVehicle.java
@@ -4,39 +4,4 @@ public interface WaterVehicle {
     public String getVehicleName();
     public int getVehicleCapacity();
 }
-class Boat implements WaterVehicle{
 
-    String name;
-    int capacity;
-
-    Boat(String name,int capacity){
-        this.name = name;
-        this.capacity = capacity;
-    }
-
-    @Override
-    public String getVehicleName() {
-        return name;
-    }
-
-    @Override
-    public int getVehicleCapacity() {
-        return capacity;
-    }
-
-    public java.lang.String getName() {
-        return name;
-    }
-
-    public void setName(java.lang.String name) {
-        this.name = name;
-    }
-
-    public int getCapacity() {
-        return capacity;
-    }
-
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
-}
